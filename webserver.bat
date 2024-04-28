d87f6b623e5156e99d873bb8db3c3e3beb093388
eggman6 <eggmansisapingas@gmail.com>
2024-04-28 12:25:21 -0400

    404 page added

--- webserver.bat
+++ webserver.bat
@@ -1,1 +1,2 @@
-http-server -S -C C:/PRIVATE/localhost.pem -K C:/PRIVATE/localhost-key.pem -p 443
+start cmd /k http-server -S -C C:/PRIVATE/localhost.pem -K C:/PRIVATE/localhost-key.pem -p 443
+start cmd /k http-server -p 80
